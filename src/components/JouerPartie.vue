9dff22c8e395952fff90d24a98bc14288e70dac3
martalexa <alexandramartinpro@gmail.com>
2018-02-05 16:03:10 +0100

    bouton partie suivante

--- src/components/JouerPartie.vue
+++ src/components/JouerPartie.vue
@@ -7,6 +7,9 @@
         <div class="column container">
           <p>Où se trouve cette photo ?</p>
           <img src="https://www.tourisme-lorraine.fr/sitlorimg/1920/0/aHR0cHM6Ly93d3cuc2l0bG9yLmZyL3Bob3Rvcy83MzcvNzM3MDAzNjg0XzE5LmpwZw==.jpg" alt="photo">
+          <a class="button is-success is-outlined">
+            Question suivante
+          </a>
         </div>
       </div>
 
@@ -37,7 +40,6 @@ export default {
 
 .carte{
   border :1px solid black;
-
   height:75vh;
 }
 
