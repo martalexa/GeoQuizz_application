556253fd6bb715983f9d3be03177077a13f5399d
DanielRicklin <ricklin.daniel@outlook.fr>
2018-02-05 10:30:47 +0100

    debut structure

---
+++ src/components/CreationPartie.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <h1>Bonjour</h1>
+
+</template>
+
+
+<script>
+//import Toolbar from './Toolbar.vue'
+export default {
+	name: 'CreationPartie',
+	data () {
+		return {
+			
+		}
+	},
+	components :{
+		//Toolbar
+	},
+	methods:{
+	
+	}
+}
+</script>
+
+
+<style scoped>
+
+</style>
